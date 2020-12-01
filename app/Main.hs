be11d1fecf2a99f253d7a805af75929654e76064
ajuvercr <arthur.vercruysse@outlook.com>
2020-12-01 08:53:22 +0100

    first day + initial setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,43 @@
 module Main where
 
+import Control.Monad
+import Control.Applicative
+import System.IO
+import System.Environment
+
+import Data.Time.Clock
+import Data.Time.Calendar
+
 import qualified Day01
 
+thdOf3 :: (a, b, c) -> c
+thdOf3 (_, _, x) = x
+
+
+date :: IO (Integer,Int,Int) -- :: (year,month,day)
+date = toGregorian . utctDay <$> getCurrentTime
+
+dayofmonth :: IO Int
+dayofmonth = thdOf3 <$> date
+
+
+parse :: [String] -> IO (Int, String)
+parse (location:xs) = liftM2 (,) (parseDay xs) (readfile location)
+parse [] = liftM2 (,) dayofmonth getContents
+
+
+parseDay :: [String] -> IO Int
+parseDay [x] = return $ read x
+parseDay _ = dayofmonth
+
+
+readfile :: String -> IO String
+readfile location = openFile location ReadMode >>= hGetContents
+
+
 main :: IO ()
-main = Day01.solve
+main = getArgs >>=  parse >>= uncurry solve
+
+
+solve :: Int -> String -> IO ()
+solve 1 = Day01.solve
--- src/Day01.hs
+++ src/Day01.hs
@@ -1,6 +1,35 @@
 module Day01
     ( solve
     ) where
+import System.IO
+import Data.List
+import Control.Applicative
 
-solve :: IO ()
-solve = putStrLn "Solve day1"
+
+parseinput :: String -> [Int]
+parseinput content = map read $ lines content
+
+
+solve1 :: Int -> [Int] -> [Int] -> Maybe Int
+solve1 target (s:ss) (l:ll)
+    | s + l < target = solve1 target ss (l:ll)
+    | s + l > target = solve1 target (s:ss) ll
+    | otherwise      = return $ s * l
+solve1 _ _ _ = Nothing
+
+
+firstJust :: (a -> Maybe b) -> [a] -> Maybe b
+firstJust f (x:xs) = f x <|> firstJust f xs
+
+
+solve :: String -> IO ()
+solve x = do
+    let input = parseinput x
+    let sorted = sort input
+    let revSorted = reverse sorted
+
+    putStr "Par 1: "
+    print $ solve1 2020 sorted revSorted
+
+    putStr "Part 2: "
+    print $ firstJust (\target -> (* target) <$> solve1 (2020 - target) sorted revSorted) input
