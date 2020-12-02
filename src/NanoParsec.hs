3d03047f4ce6e1174e3577bc2e91cf1bede5cf65
ajuvercr <arthur.vercruysse@outlook.com>
2020-12-02 14:17:13 +0100

    add docs

--- src/NanoParsec.hs
+++ src/NanoParsec.hs
@@ -21,6 +21,7 @@ item = Parser $ \case
    []     -> []
    (c:cs) -> [(c,cs)]
 
+
 bind :: Parser a -> (a -> Parser b) -> Parser b
 bind p f = Parser $ \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
 
@@ -46,12 +47,18 @@ instance Alternative Parser where
   empty = mzero
   (<|>) = option
 
+
+-- |Parse a or b
 combine :: Parser a -> Parser a -> Parser a
 combine p q = Parser (\s -> parse p s ++ parse q s)
 
+
+-- |Parse failure
 failure :: Parser a
 failure = Parser (const [])
 
+
+-- |Parse a or b
 option :: Parser a -> Parser a -> Parser a
 option  p q = Parser $ \s ->
   case parse p s of
@@ -59,6 +66,7 @@ option  p q = Parser $ \s ->
     res    -> res
 
 
+-- |Parse a character satisfying something
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = item `bind` \c ->
   if p c
@@ -66,6 +74,7 @@ satisfy p = item `bind` \c ->
   else failure
 
 
+-- |Parse a character from [Char]
 oneOf :: String -> Parser Char
 oneOf s = satisfy (`elem` s)
 
@@ -74,6 +83,7 @@ chainl :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
 chainl p op a = (p `chainl1` op) <|> return a
 
 
+-- |Chain parsers p and reduce with op
 chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
 p `chainl1` op = do {a <- p; rest a}
   where rest a = (do f <- op
@@ -82,39 +92,48 @@ p `chainl1` op = do {a <- p; rest a}
                  <|> return a
 
 
+-- |Parse a specific character
 char :: Char -> Parser Char
 char c = satisfy (c ==)
 
 
-str :: Parser String
-str = some (satisfy isAlpha)
-
-
-natural :: Parser Integer
-natural = read <$> some (satisfy isDigit)
-
-
+-- |Parse a specific String
 string :: String -> Parser String
 string [] = return []
 string (c:cs) = do { char c; string cs; return (c:cs)}
 
 
+-- |Parse some alpha charater sequence
+str :: Parser String
+str = some (satisfy isAlpha)
+
+
+-- |Parse some digit sequence as Integer
+natural :: Parser Integer
+natural = read <$> some (satisfy isDigit)
+
+
+-- |Parse something followed by whitespace
 token :: Parser a -> Parser a
 token p = do { a <- p; spaces ; return a}
 
 
+-- |Parse a string followed by whitespace
 reserved :: String -> Parser String
-reserved s = token (string s)
+reserved s = token $ string s
 
 
+-- |Parse much whitespace
 spaces :: Parser String
 spaces = many $ oneOf " \n\r"
 
 
+-- |Parse one digit
 digit :: Parser Char
 digit = satisfy isDigit
 
 
+-- |Parse some digit sequence as Int
 number :: Parser Int
 number = do
   s <- string "-" <|> return []
@@ -122,21 +141,10 @@ number = do
   return $ read (s ++ cs)
 
 
+-- |Parse a something inside parentheses
 parens :: Parser a -> Parser a
 parens m = do
   reserved "("
   n <- m
   reserved ")"
   return n
-
-
--- BNF >>
---
--- number = [ "-" ] digit { digit }.
--- digit  = "0" | "1" | ... | "8" | "9".
--- expr   = term { addop term }.
--- term   = factor { mulop factor }.
--- factor = "(" expr ")" | number.
--- addop  = "+" | "-".
--- mulop  = "*".
---
