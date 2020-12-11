dc8692c0c0fdcd1977c717bb005dc3e3d25e33c6
ajuvercr <arthur.vercruysse@outlook.com>
2020-12-11 12:42:42 +0100

    PARALLEL

--- src/Day11.hs
+++ src/Day11.hs
@@ -7,10 +7,12 @@ module Day11
 import Lib
 import Data.Maybe
 import qualified Data.List
-import Data.Set (Set, empty, union, fromList, filter, difference)
+import Data.List.Ordered
+import qualified Data.Map.Strict as M
+-- import Data.Set (Set, empty, union, fromList, filter, difference, fromAscList)
+import Control.Parallel.Strategies
 
 type Coord = (Int, Int)
-type FilterF = Set Coord -> Coord -> Bool
 type AdjF = Coord -> [Coord]
 
 (a1, a2) `plus` (b1, b2) = (a1+b1, a2+b2)
@@ -20,11 +22,16 @@ dirs :: [Coord]
 dirs = [(1,0), (0,1), (-1,0), (0, -1), (1,1), (-1, -1), (1, -1), (-1,1)]
 
 
+buildAdj :: AdjF -> [Coord] -> M.Map Coord [Coord]
+buildAdj f cs = M.fromAscList $ map b cs
+    where b x = (x, f x)
+
+
 adjacent :: AdjF
 adjacent x = map (x `plus` ) dirs
 
 
-adjacent' :: Set Coord -> AdjF
+adjacent' :: [Coord] -> AdjF
 adjacent' all x = mapMaybe (foo all x) dirs
     where
         foo all x dx
@@ -34,48 +41,33 @@ adjacent' all x = mapMaybe (foo all x) dirs
             where nx@(j, k) = x `plus` dx
 
 
-staysempty :: AdjF -> FilterF
-staysempty adj xs x = any (`elem` xs) (adj x)
+toMaybe True a = Just a
+toMaybe False _ = Nothing
 
 
-staysfull :: Int -> AdjF -> FilterF
-staysfull treshhold adj xs x = treshhold > count (`elem` xs) (adj x)
+pmap f xs = map f xs `using` parList rdeepseq
 
 
-simulate :: FilterF -> FilterF -> Set Coord -> (Set Coord, Set Coord) -> (Set Coord, Set Coord)
-simulate e f all (vacent, occup)= (va, all `difference` va)
+sim :: Int -> [Coord] -> AdjF -> [Coord] -> [Coord]
+sim thresh all adj occ = catMaybes $ pmap getFilter all  -- ENH: Can this go parallel?
     where
-        va1 = Data.Set.filter (e occup) vacent
-        va2 = Data.Set.filter (not . f occup) occup
-        va = va1 `union` va2
-
-
-simulate' :: FilterF -> FilterF -> Set Coord -> (Set Coord, Set Coord) -> Int
-simulate' e f all x
-    | x == x' = length $ snd x
-    | otherwise = simulate' e f all x'
-    where x' = simulate e f all x
-
-
-sim :: Int -> Set Coord -> AdjF -> Set Coord -> Set Coord
-sim thresh all adj occ = Data.Set.filter getFilter all
-    where
+        isocc = has occ
         getFilter x
-            | x `elem` occ = c (thresh >) (adj x)   -- Occ seat stays occ if thresh is bigger
-            | otherwise    = c (0 ==)     (adj x)   -- Emtpy seat changes if no adj seats are in occ
-        c f = f . count (`elem` occ)
+            | isocc x   = toMaybe (thresh > c x) x   -- Occ seat stays occ if thresh is bigger
+            | otherwise = toMaybe (0 ==     c x) x   -- Emtpy seat changes if no adj seats are in occ
+        c x = count isocc (adj x)
+
 
 run :: Eq a => (a -> a) -> a -> a
 run f a
-    | a == a' = a
+    | a == a'   = a
     | otherwise = run f a'
     where a' = f a
 
 
-
-type Prep = Set Coord
-getcoords :: [String] -> Set Coord
-getcoords x = fromList $ getcoords' 0 x
+type Prep = [Coord]
+getcoords :: [String] -> [Coord]
+getcoords x = Data.List.sort $ getcoords' 0 x
     where
         getcoords' _ [] = []
         getcoords' at (x:xs) = map (,at) (Data.List.elemIndices 'L' x) ++ getcoords' (at+1) xs
@@ -86,17 +78,16 @@ prepare = getcoords . lines
 
 part1 :: Prep -> IO ()
 part1 coord = putStr "Part 1: " >> print (length $ run r coord)
-    where r = sim 4 coord adjacent
+    where
+        m = buildAdj adjacent coord
+        r = sim 4 coord (m M.!)
 
 
 part2 :: Prep -> IO ()
 part2 coord = putStr "Part 2: " >> print (length $ run r coord)
-    where r = sim 5 coord (adjacent' coord)
--- part2 coords = putStr "Part 2: " >> print (simulate' e f coords (coords, empty))
---     where
---         adj = adjacent' coords
---         e = staysempty adj
---         f = staysfull 5 adj
+    where
+        m = buildAdj (adjacent' coord) coord
+        r = sim 5 coord (m M.!)
 
 
 solve :: Maybe Int -> String -> IO ()
