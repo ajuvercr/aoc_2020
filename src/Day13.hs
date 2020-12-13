35002b860862c543905dc220f021948601761001
ajuvercr <arthur.vercruysse@outlook.com>
2020-12-13 13:44:04 +0100

    add day 13 part 2

--- src/Day13.hs
+++ src/Day13.hs
@@ -2,29 +2,53 @@ module Day13
     (solve
     ) where
 
-import Lib
 import NanoParsec ( Parser, runParser, char, string, star, number )
-import Control.Applicative
+import Control.Applicative ( Alternative((<|>)) )
 import Data.Functor (($>))
-import Data.Maybe
+import Data.Maybe ( mapMaybe )
 
-data Bus = Time Int
+data Bus = Time Integer
          | X
          deriving (Show)
+
+
+modInv :: Integer -> Integer -> Integer
+modInv a m
+  | 1 == g = mkPos i
+  where
+    (i, _, g) = gcdExt a m
+    mkPos x
+      | x < 0 = x + m
+      | otherwise = x
+
+
+gcdExt :: Integer -> Integer -> (Integer, Integer, Integer)
+gcdExt a 0 = (1, 0, a)
+gcdExt a b =
+  let (q, r) = a `quotRem` b
+      (s, t, g) = gcdExt b r
+  in (t, s - q * t, g)
+
+
+step :: (Integer, Integer) -> Bus -> (Integer, Integer)
+step x X = step x (Time 1)
+step (g, o) (Time x) = (g * x, x * tok g x (o + 1))
+    where tok x y o = (o * modInv y x) `mod` x
+
 
 parseBus :: Parser Bus
 parseBus = (time <|> none) <* (char ',' $> () <|> return ())
     where
-        time = Time <$> number
+        time = Time . toInteger <$> number
         none = X <$ string "x"
 
 
-filterPart :: Int -> Bus -> Maybe (Int, Int)
+filterPart :: Integer -> Bus -> Maybe (Integer, Integer)
 filterPart _ X        = Nothing
 filterPart x (Time t) = Just (t - x `mod` t, t)
 
 
-type Prep = (Int, [Bus])
+type Prep = (Integer, [Bus])
 prepare :: String -> Prep
 prepare x = let h:hs:_ = lines x
             in (read h, runParser (star parseBus) hs)
@@ -35,7 +59,10 @@ part1 (target, busses) = putStr "Part 1: " >> print (uncurry (*) (foldl min (tar
 
 
 part2 :: Prep -> IO ()
-part2 x = putStr "Part 2: "
+part2 (_, busses) = putStr "Part 2: " >> print sol
+    where
+        l = toInteger $ length busses
+        sol = 1 - l + snd (foldl step (1, 0) busses)
 
 
 solve :: Maybe Int -> String -> IO ()
