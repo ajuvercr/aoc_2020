18c73be6c23679bb16fd7400cedac6e312a14108
ajuvercr <arthur.vercruysse@outlook.com>
2020-12-14 21:08:15 +0100

    do part 2, but almost twice maybe?

--- src/Day14.hs
+++ src/Day14.hs
@@ -9,15 +9,106 @@ import NanoParsec
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import Debug.Trace
+import Control.Applicative
 
-type Mask = (Int, Int, Mask2)
+import Data.Char (digitToInt)
+import Data.List (foldl')
+
+type Mask = ((Int, Int, Mask2), [(Int, Int)])
+
 
 data Action = Store Int Int
             | Mask Mask
 
 
 type Mask2 = String
--- data MaskMask =
+
+newtype Masker = Masker { valid :: Mask2 -> (Int, Mask2) }
+
+toDec :: String -> Int
+toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+
+
+orElse :: Maybe a -> a -> a
+orElse Nothing  x = x
+orElse (Just x) _ = x
+
+orElse' :: Maybe a -> a -> Maybe a
+orElse' Nothing x = Just x
+orElse' x _ = x
+
+-- REVERSED!
+tobitstring :: Int -> String
+tobitstring 0 = ""
+tobitstring x = show d ++ tobitstring rest
+    where (rest, d) = x `divMod` 2
+
+
+tomask :: Int -> Mask2
+tomask x = r ++ replicate (36 - l) '0'
+    where
+        r = tobitstring x
+        l = length r
+
+
+withMask :: Mask2 -> Mask2 -> Mask2
+withMask [] [] = []
+withMask (x:xs) (y:ys)
+    | y == 'X' = 'X' : withMask xs ys
+    | y == '1' = '1' : withMask xs ys
+    | otherwise = x  : withMask xs ys
+
+
+other :: Char -> Char
+other '0' = '1'
+other '1' = '0'
+
+
+applyMask :: Mask2 -> Mask2 -> Maybe Mask2
+applyMask [] [] = Nothing
+applyMask (x:xs) (m:ms)
+    | m == 'X'  = (x:) <$> applyMask xs ms
+    | x == 'X'  = (('X':) <$> applyMask xs ms) `orElse'` (other m:xs)
+    | x == m    = (x:) <$> applyMask xs ms
+    | otherwise = Just (x:xs)
+
+
+applyMasks :: [Mask2] -> Mask2 -> Maybe Mask2
+applyMasks [] x = Just x
+applyMasks (m:ms) x = applyMask x m >>= applyMasks ms
+
+
+dopart2' :: Mask2 -> (Int, Int) -> (Int, [Mask2]) -> (Int, [Mask2])
+dopart2' m (k, v) (s, ms) = newMask `orElse` (s, ms)
+    where
+        mask = withMask (tomask k) m
+        domask m = (s + (2 ^ count (=='X') m) * v, m:ms)
+        newMask = domask <$> applyMasks (reverse ms) mask
+
+
+domasks :: Mask2 -> [Mask2]
+domasks [] = [""]
+domasks (m:ms)
+    | m == 'X' = map ('0':) rest ++ map ('1':) rest
+    | otherwise = map (m:) rest
+    where rest = domasks ms
+
+
+dopart2'' :: Mask2 -> M.Map Int Int -> (Int, Int) -> M.Map Int Int
+dopart2'' mask m (k, v) = install m (domasks k'')
+    where
+        k'' = withMask (tomask k) mask
+        install s [] = s
+        install s (x:xs) = M.insert (toDec x) v (install s xs)
+
+
+dopart2 :: [Mask] -> (Int, [Mask2])
+dopart2 [] = (0, [])
+dopart2 (((_, _, mask), mses):ms) = foldr (dopart2' mask) (dopart2 ms) (reverse mses)
+
+
+dopart22 :: M.Map Int Int -> Mask -> M.Map Int Int
+dopart22 s ((_, _, mask), mses) = foldl (dopart2'' mask) s mses
 
 
 toint :: (a -> Bool) -> [a] -> Int
@@ -27,49 +118,43 @@ toint p (x:xs)
     | otherwise = 2 * toint p xs
 
 
-
-
-parseStore :: Parser Action
+parseStore :: Parser (Int, Int)
 parseStore = do
     string "mem["
     loc <- number
     string "] = "
-    Store loc <$> number <* spaces
+    (,) loc <$> number <* spaces
 
 
-parseMask :: Parser Action
+parseMask :: Parser Mask
 parseMask = do
     string "mask = "
     v <- reverse <$> strAll
     spaces
-    return $ Mask (toint (=='1') v, complement (toint (=='0') v), v)
+    xs <- plus parseStore
+    return ((toint (=='1') v, complement (toint (=='0') v), v), xs)
 
 
-doStep :: (M.Map Int Int, Mask) -> Action -> (M.Map Int Int, Mask)
-doStep (s, (ls, os, xs)) (Store at v) = (M.insert at v' s , (ls, os, xs))
-    where v' = os .&. v .|. ls
-doStep (s, _) (Mask x) = (s, x)
+doStep :: M.Map Int Int -> Mask -> M.Map Int Int
+doStep s ((ls, os, _), xs) = foldl ins s xs
+    where
+        ins s (at, v) = M.insert at (mask v) s
+        mask v = os .&. v .|. ls
 
 
-doStep2 :: (M.Map Int Int, Mask) -> Action -> (M.Map Int Int, Mask)
-doStep2 (s, _) (Mask x) = (s, x)
--- doStep2 (s, (ls, os, xs)) (Store at v) = (setAll at' v xs s, (ls, os, xs))
---     where at' = os .&. at .|. ls
-
-
-type Prep = [Action]
+type Prep = [Mask]
 prepare :: String -> Prep
-prepare = runParser (star (parseStore `option` parseMask))
+prepare = runParser (star parseMask)
 
 
 part1 :: Prep -> IO ()
-part1 x = putStr "Part 1: " >> print (sum (fst sol))
-    where sol = Prelude.foldl doStep (M.empty, (0, 0, [])) x
+part1 x = putStr "Part 1: " >> print (sum (M.elems sol))
+    where sol = Prelude.foldl doStep M.empty x
 
 
 part2 :: Prep -> IO ()
-part2 x = putStr "Part 2: " >> print (sum (fst sol))
-    where sol = Prelude.foldl doStep2 (M.empty, (0, 0, [])) x
+part2 x = putStr "Part 2: " >> print (sum (M.elems sol))
+    where sol = foldl dopart22 M.empty x
 
 
 
