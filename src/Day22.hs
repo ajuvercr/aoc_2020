521cbd7c1f3fbc0e7f1313ef5061166385141981
ajuvercr <arthur.vercruysse@outlook.com>
2020-12-22 12:14:52 +0100

    finish day 22

--- src/Day22.hs
+++ src/Day22.hs
@@ -4,8 +4,8 @@ module Day22
 
 import Lib
 import NanoParsec
-import Debug.Trace
 import qualified Data.Set as S
+
 
 finish :: [Int] -> Int
 finish x = sum (zipWith (*) [1..] $ reverse x)
@@ -27,41 +27,26 @@ play1 (x, [])      = x
 play1 ([], y)      = y
 play1 (x:xs, y:ys)
     | x > y = play1 (xs ++ [x, y], ys)
-    | y > x = play1 (xs, ys ++ [y, x])
+    | y > x = play1 (xs,           ys ++ [y, x])
 
 -- True = Player 1 wins, False == Player 2 wins
 subGame :: ([Int], [Int]) -> Bool
-subGame x = snd $ play2 (S.empty, S.empty) x
+subGame x = snd $ play2 S.empty x
 
 
-play2 :: (S.Set [Int], S.Set [Int]) -> ([Int], [Int]) -> ([Int], Bool)
+play2 :: S.Set ([Int], [Int]) -> ([Int], [Int]) -> ([Int], Bool)
 play2 dones (x, [])      = (x, True)
 play2 dones ([], y)      = (y, False)
-play2 (xd, yd) (x:xs, y:ys)
-    | S.member (x:xs) xd || S.member (y:ys) yd = play2 (S.insert nxs xd, yd) (nxs, ys)
-    | n                              = play2Normal (xd, yd) (x:xs, y:ys)
-    | otherwise                      = play2Sub (xd, yd) (x:xs, y:ys)
+play2 dones (xs, ys) | S.member (tail xs, tail ys) dones = (xs, True)
+play2 dones (x:xs, y:ys) = play2 newstate $
+    if' n
+        (if' (x > y) (nxs, ys) (xs , nys))
+        (if' (subGame (take x xs, take y ys)) (nxs, ys) (xs , nys))
     where
         n = x > length xs || y > length ys
         nxs = xs ++ [x, y]
-
-
-play2Normal :: (S.Set [Int], S.Set [Int]) -> ([Int], [Int]) -> ([Int], Bool)
-play2Normal (xdone, ydone) (x:xs, y:ys)
-    | x > y     = play2 (S.insert (x:xs) xdone, ydone) (nxs, ys)
-    | otherwise = play2 (xdone, S.insert (y:ys) ydone) (xs , nys)
-    where
-        nxs = xs ++ [x, y]
         nys = ys ++ [y, x]
-
-
-play2Sub :: (S.Set [Int], S.Set [Int]) -> ([Int], [Int]) -> ([Int], Bool)
-play2Sub (xdone, ydone) (x:xs, y:ys)
-    | subGame (xs, ys) = play2 (S.insert (x:xs) xdone, ydone) (nxs, ys)
-    | otherwise        = play2 (xdone, S.insert (y:ys) ydone) (xs , nys)
-    where
-        nxs = xs ++ [x, y]
-        nys = ys ++ [y, x]
+        newstate = S.insert (xs, ys) dones
 
 
 part1 :: Prep -> IO ()
@@ -71,7 +56,7 @@ part1 x = putStr "Part 1: " >> print (finish played)
 
 part2 :: Prep -> IO ()
 part2 x = putStr "Part 2: " >> print (finish $ fst played)
-    where played = play2 (S.empty, S.empty) x
+    where played = play2 S.empty x
 
 
 solve :: Maybe Int -> String -> IO ()
