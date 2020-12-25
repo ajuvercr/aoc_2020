a1b1db909c6df1b26f3a2572933e2dd96de720bc
ajuvercr <arthur.vercruysse@outlook.com>
2020-12-25 09:33:21 +0100

    indeed

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections #-}
-
 module Day25
     (solve
     ) where
@@ -7,15 +5,17 @@ module Day25
 import Lib
 
 transform :: Int -> Int -> Int
-transform = curry ((`mod` 20201227) . uncurry (*))
+transform sub self = self * sub `mod` 20201227
 
-loops :: Int -> (Int, Int)
-loops key = fromLeft $ loops' 1 (map (1,) [1..])
-    where
-        loops' count state = tloop count state >>= loops' (count+1)
-        tloop 0 xs = Right xs
-        tloop c (x:xs) = (:) <$> goodOrTransform c x <*> tloop (c-1) xs
-        goodOrTransform c (self, sub) = let self' = transform sub self in if' (self' == key) (Left (sub, c)) (Right (self', sub))
+
+subj :: Int
+subj = 7
+
+
+-- | calculates the loopsize for some public key, subject number and itself
+loopSize :: Int -> Int -> Int
+loopSize key self = if' (val == key) 1 (1 + loopSize key val)
+    where val = transform subj self
 
 
 type Prep = (Int, Int)
@@ -25,19 +25,12 @@ prepare inp = (read x, read y)
 
 
 part1 :: Prep -> IO ()
-part1 (p1, p2) = putStr "Part 1: " >> print (iterate (transform p2) 1 !! l1)
-    where
-        (s1, l1) = loops p1
-        (s2, l2) = loops p2
-    -- where loopSize =
+part1 (p1, p2) = putStr "Part 1: " >> print (iterate (transform p2) 1 !! loops)
+    where loops = loopSize p1 1
 
 
 part2 :: Prep -> IO ()
-part2 x = putStr "Part 2: " >> print secret -- , secret == 14897079)
-    where
-        (s1, l1) = loops 5764801
-        (s2, l2) = loops 17807724
-        secret = take (l1+1) $ iterate (transform 17807724) 1
+part2 x = print "Part 2: "
 
 
 solve :: Maybe Int -> String -> IO ()
